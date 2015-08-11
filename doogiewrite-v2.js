6e8442fb12bbd34bb3ddda18485afbc027706065
Nick Santaniello <nsantaniello@madwellnyc.com>
2015-08-11 15:28:24 -0400

    Changed note image to a sprite, added additional styles, continued to pare down doogiewrite.js

--- doogiewrite-v2.js
+++ doogiewrite-v2.js
@@ -7,7 +7,7 @@ var Doogiewrite = {
     init : function(){
       
         // Add event listener for tab workaround (tab key should produce several spaces, not change fields)
-        $('#doc-body').keydown(function (e) {
+        $('#user-document').keydown(function (e) {
         
             if (e.keyCode == 9) {
                 var myValue = "\t";
@@ -29,76 +29,70 @@ var Doogiewrite = {
     },
 
     clearTextArea: function(){
-        var theDoc = document.getElementById("doc-body");
+        var theDoc = document.getElementById("user-document");
         theDoc.value = "";
     },
 
     checkPrefs : function(){
      
         // Load the locally stored autosave preference
-        Doogiewrite.savePref = localStorage.getItem("savePref")
+        this.savePref = localStorage.getItem("savePref")
        
         // If the save preference hasn't been set, assume the visitor is new and load the welcome message.
-        if(Doogiewrite.savePref==null){
-            var theDoc = document.getElementById("doc-body");
-            theDoc.value = "Welcome to DoogieWrite!\n\nDoogieWrite is a retro-inspired web-based text editor, just click on the screen and start typing to begin.\n\nIf you're running Chrome, you can hit command-shift-F to enter full-screen mode.\n\nThis message won't show the next time you load this page, but you can also hit refresh to clear this text.\n\nVERSION NOTES: After clicking SAVE, your .txt file will appear in another browser window where you can then save it to disk through File -> Save." ;
+        if(this.savePref==null){
+            var theDoc = document.getElementById("user-document");
+            theDoc.value = "Welcome to this!\n\nthis is a retro-inspired web-based text editor, just click on the screen and start typing to begin.\n\nIf you're running Chrome, you can hit command-shift-F to enter full-screen mode.\n\nThis message won't show the next time you load this page, but you can also hit refresh to clear this text.\n\nVERSION NOTES: After clicking SAVE, your .txt file will appear in another browser window where you can then save it to disk through File -> Save." ;
             // Autosave, by default, is off.
-            Doogiewrite.savePref=0;
-            localStorage.setItem("savePref", Doogiewrite.savePref);
+            this.savePref=0;
+            localStorage.setItem("savePref", this.savePref);
 
         // If the save preference is set to off, load an empty document
-        } else if (Doogiewrite.savePref==0){
+        } else if (this.savePref==0){
     
             // If there's no autosave set, clear the text area.
-            var autoSaveButton = document.getElementById("autosaveButton");
+            var autoSaveButton = document.getElementById("autosave-button");
             autoSaveButton.innerHTML = "AUTOSAVE OFF"
             this.clearTextArea();
         }
      
         // If autosave box is set, load the text file from localStorage
-        else if (Doogiewrite.savePref==1){
-            var box = document.getElementById("box");
-            box.classList.add('checked');
+        else if (this.savePref==1){
+            this.addCheckbox("autosave-toggle-box");
 
             var loadedText = localStorage.getItem("storedDoc")
-            document.getElementById("doc-body").value = loadedText;
+            document.getElementById("user-document").value = loadedText;
             
             // Add listener to save after every key stroke.
             document.addEventListener("keyup", this.saveLocal, false);
-            var autoSaveButton = document.getElementById("autosaveButton");
+            var autoSaveButton = document.getElementById("autosave-button");
             autoSaveButton.innerHTML = "AUTOSAVE ON"
         }
 
         // Add listeners for buttons
         
-        var saveButton = document.getElementById("saveButton");
+        var saveButton = document.getElementById("save-button");
         saveButton.addEventListener("click", this.saveFile, false);
 
-        var loadButton = document.getElementById("loadButton");
-        loadButton.addEventListener("click", this.load, false);
+        var box = document.getElementById("autosave-toggle-box");
+        box.addEventListener("click", this.toggleCheck, false);
 
-        var box = document.getElementById("box");
-        box.addEventListener("click", Doogiewrite.toggleCheck, false);
+        var autoSaveButton = document.getElementById("autosave-button");
+        autoSaveButton.addEventListener("click", this.toggleCheck, false);
 
-        var autoSaveButton = document.getElementById("autosaveButton");
-        autoSaveButton.addEventListener("click", Doogiewrite.toggleCheck, false);
-
-        var emailButton = document.getElementById("emailButton");
+        var emailButton = document.getElementById("email-button");
         emailButton.addEventListener("click", this.email, false);
 
-        var musicBox = document.getElementById("musicbox");
-        musicBox.addEventListener("click", Doogiewrite.toggleMusic, false);
+        var musicBox = document.getElementById("music-toggle");
+        musicBox.addEventListener("click", this.toggleMusic, false);
 
         // set the browser focus to the main editor screen
-        var documentArea = document.getElementById("doc-body");
+        var documentArea = document.getElementById("user-document");
         documentArea.focus();
     },
 
     saveFile: function(){
-        var documentText = document.getElementById("doc-body").value;
-        var data = {
-            "txt" : documentText
-        };
+        var documentText = document.getElementById("user-document").value;
+      
         // Javascript does not allow creating files on the clientside. Run a simple php script to return the document text as a text file.
         document.location.href = "save.php?txt=" + documentText;
     },
@@ -107,27 +101,36 @@ var Doogiewrite = {
 
         if(Doogiewrite.musicPref==0){
             Doogiewrite.musicPref=1;
-            var mbox = document.getElementById("musicbox");
-            mbox.classList.add("checked");
+            Doogiewrite.addCheckbox("music-toggle")
         
-            // Change the music button image
-            $("#notePic").attr('src', "note1.png")
-        
-            var audioPlayer = document.getElementById("audioPl")
+            var audioPlayer = document.getElementById("audio-player")
             audioPlayer.play();  
         } else if (Doogiewrite.musicPref==1){
             Doogiewrite.musicPref=0;
-            var musicbox = document.getElementById("musicbox");
-            musicbox.classList.remove('checked');
-            var audioPlayer = document.getElementById("audioPl")
-            $("#notePic").attr('src', "note2.png")
+            // var musicbox = document.getElementById("music-toggle");
+            // musicbox.classList.remove('checked');
+            Doogiewrite.removeCheckbox("music-toggle");
+            var audioPlayer = document.getElementById("audio-player")
+            
+            // Reset the audio player to the beginning.
             audioPlayer.pause();  
+            audioPlayer.currentTime = 0;
         }
+    },
+
+    removeCheckbox : function(element){
+        var theElement = document.getElementById(element);
+        theElement.classList.remove('checked');
+    },
+
+    addCheckbox : function(element){
+        var theElement = document.getElementById(element);
+        theElement.classList.add('checked');
     },
 
     email: function(){
         //Concert the document text to html and send it via a mailto link.
-        var currentDoc = document.getElementById("doc-body").value;
+        var currentDoc = document.getElementById("user-document").value;
         var addy = prompt("Enter your email address.");
 
         // Replace linebreaks with <br> tags.
@@ -149,25 +152,23 @@ var Doogiewrite = {
             //set save document pref to on
             localStorage.setItem("savePref", Doogiewrite.savePref);
      
-            var autosaveButton = document.getElementById("autosaveButton");
+            var autosaveButton = document.getElementById("autosave-button");
             autoSaveButton.innerHTML = "AUTOSAVE ON"
         }
         // Otherwise, uncheck it, turn off local storage listener and save preference off
         else if (this.classList.contains("checked")){
             Doogiewrite.savePref=0;
-            var box = document.getElementById("box");
-            this.classList.remove("checked");
+            Doogiewrite.removeCheckbox("autosave-toggle-box");
             //Remove autosave keyup listener
             document.removeEventListener("keyup", Doogiewrite.saveLocal, false);
             localStorage.setItem("savePref", 0);
       
-            var autoSaveButton = document.getElementById("autosaveButton");
+            var autoSaveButton = document.getElementById("autosave-button");
             autoSaveButton.innerHTML = "AUTOSAVE OFF";
         }
     },
 
     fileImport : function(files) {
-
         confirm("Are you sure you want to load the selected file? This will overwrite any unsaved text that you are working on.");
         var file = files[0];
         
@@ -176,12 +177,12 @@ var Doogiewrite = {
         fileReader.readAsText(file);
     
         fileReader.onload = function(evt) {
-            document.getElementById("doc-body").value = event.target.result;
+            document.getElementById("user-document").value = event.target.result;
         }
     },
 
     saveLocal : function(){
-        var currentDoc = document.getElementById("doc-body").value;
+        var currentDoc = document.getElementById("user-document").value;
         localStorage.setItem("storedDoc", currentDoc);
     }
 };
