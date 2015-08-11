c87989b9ab2073f14a17fdf8bf12449262a0c2ff
Nick Santaniello <nsantaniello@madwellnyc.com>
2015-08-11 15:29:35 -0400

    Remove superfluous files

--- save.php
+++ save.php
@@ -2,14 +2,12 @@
 error_reporting(E_ALL);
 $txt = $_GET["txt"];
 $txt_new = stripslashes($txt);
-$test = "woah";
 
 header("Status: 200");
 header("Content-type: application/doogiewrite");
 header("Content-Disposition:attachment; filename=\"my_text_file.txt\"");
 
 echo $txt_new;
-
 
 exit();
 ?>
